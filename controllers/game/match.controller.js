7bbfc5810b791eed04364c16c753f877723c5a22
Sepehr Norouzi <s.norouzi@medrick.info>
2023-07-23 17:43:09 +0330

    main-005: Init board and turn.

--- controllers/game/match.controller.js
+++ controllers/game/match.controller.js
@@ -1,5 +1,17 @@
 const match_wait = [];
 const { v4: uuidv4 } = require('uuid');
+
+const init_room = (io, match_id) => {
+    const room = io.of('/').adapter.rooms.get(match_id);
+    const BOARD_SIZE = 3;
+    const board = [];
+    for (let i = 0; i < BOARD_SIZE; i++) {
+        board.push(Array.from({ length: BOARD_SIZE }, () => 0));
+    }
+    room['turn'] = 1;
+    room['board'] = board;
+}
+
 exports.match_make = (socket, io) => {
   match_wait.push(socket);
 
@@ -9,7 +21,9 @@ exports.match_make = (socket, io) => {
     const p2 = match_wait.pop();
     p1.join(match_id);
     p2.join(match_id);
-    io.to(match_id).emit("match_made", {room: match_id})
+    init_room(io, match_id);
+    const room =  io.of('/').adapter.rooms.get(match_id);
+    io.to(match_id).emit("match_made", {room: match_id, board: room.board, turn: room.turn});
   }
   
 };
